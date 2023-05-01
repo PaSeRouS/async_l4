4ed6c30c97e7c316b228f0d1e2d1da6fadccd2b1
PaSeRouS <paser96@mail.ru>
2023-05-01 21:42:33 +0300

    Save messages history to file

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import asyncio
+import datetime
+
+import aiofiles
 
 
 async def tcp_chat():
@@ -7,9 +10,22 @@ async def tcp_chat():
         5000
     )
 
+    today = datetime.datetime.now()
+    today_str = today.strftime('%d.%m.%Y %H:%M')
+    print(f'[{today_str}] Установлено соединение!')
+
     while not reader.at_eof():
+        today = datetime.datetime.now()
+        today_str = today.strftime('%d.%m.%Y %H:%M')
+
         data = await reader.readline()
-        print(data.decode())
+        data = data.decode()
+
+        output = f'[{today_str}] {data}'
+        print(output)
+
+        async with aiofiles.open('chat.txt', 'a') as chat_file:
+            await chat_file.write(output)
 
 
 def main():
